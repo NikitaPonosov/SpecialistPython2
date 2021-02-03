307c63b376e2676aa1c2cde84338dbeb6534da05
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2021-02-03 12:09:04 +0300

    Update 03_task_sort.py

--- Module5/practice/03_task_sort.py
+++ Module5/practice/03_task_sort.py
@@ -1,3 +1,9 @@
 # Сумма наибольших
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов
+list1 = gen_list(20, -10, 10)
+sort(list1)
+print(list1)
+print(f"summax = {sum(list1[-1:-11:-1])}")
+# quick_sort(a)
+# print(a)
